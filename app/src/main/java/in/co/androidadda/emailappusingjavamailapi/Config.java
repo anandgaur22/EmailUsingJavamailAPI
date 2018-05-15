dc8246e7cfc4692d025bc0c8360c790d825ff22b
anand <anandgaur22@gmail.com>
2018-05-15 15:24:02 +0530

    new java mail api

---
+++ app/src/main/java/in/co/androidadda/emailappusingjavamailapi/Config.java
@@ -0,0 +1,7 @@
+package in.co.androidadda.emailappusingjavamailapi;
+
+public class Config {
+
+    public static final String EMAIL ="youremailid";
+    public static final String PASSWORD ="yourpassword";
+}
---
+++ app/src/main/java/in/co/androidadda/emailappusingjavamailapi/MainActivity.java
@@ -0,0 +1,55 @@
+package in.co.androidadda.emailappusingjavamailapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Declaring EditText
+    private EditText editTextEmail;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
+
+    //Send button
+    private Button buttonSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing the views
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextSubject = (EditText) findViewById(R.id.editTextSubject);
+        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        //Adding click listener
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sendEmail();
+
+            }
+        });
+
+    }
+
+    private void sendEmail() {
+        //Getting content for email
+        String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
+    }
+}
